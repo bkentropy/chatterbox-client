4b1ffb2bba62d0e7c8807f3eeb8ae39b298fa23d
Brian Kustra <bkustrakb@gmail.com>
2015-10-19 17:08:01 -0500

    Prompt working.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,16 @@
 // YOUR CODE HERE:
 
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent. Data: ', data);
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message. Error: ', data);
+  }
+});
